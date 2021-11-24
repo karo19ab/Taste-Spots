9e93598145ce0a46b8b8d552527bfc01a8128f41
stinenorklitlonborg <stlo19ab@student.cbs.dk>
2021-11-24 15:28:54 +0100

    Ikke noget særligt

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -23,29 +23,6 @@ const ProfileScreen = (props) => {
     //useState til at vise ratings på profilside.
     const [ratings, setRatings] = useState('');
 
-    //Her oprettes bruger state variblen
-   // const [user, setUser] = useState({loggedIn: false});
-
-    //onAuthstatechanged er en prædefineret metode, forsynet af firebase, som konstant observerer brugerens status (logget ind vs logget ud)
-    //Pba. brugerens status foretages et callback i form af setUSer metoden, som håndterer user-state variablens status.
-  /*  function onAuthStateChange(callback) {
-        return firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                callback({loggedIn: true, user: user});
-            } else {
-                callback({loggedIn: false});
-            }
-        });
-    }
-*/
-    //Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
-    /*useEffect(() => {
-        const unsubscribe = onAuthStateChange(setUser);
-        return () => {
-            unsubscribe();
-        };
-    }, []);*/
-
     //handleLogout håndterer log ud af en aktiv bruger.
     //Metoden er en prædefineret metode, som firebase stiller tilrådighed
     //Metoden er et asynkrontkald.
